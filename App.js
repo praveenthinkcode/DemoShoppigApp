57babbbcdcf68575981902a8b13fdbdb0f0f20b1
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-07-14 11:00:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'react-native-gesture-handler'
+import MainRoute from "./Navigation/MainRoute.js";
+console.disableYellowBox = true;
+export default function App() {
+  return (
+       <MainRoute /> 
+  )
+}
