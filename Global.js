4113424f80ee65a97edf9f1333ad92a53df0f3fe
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-07-14 10:59:10 +0530

    Add files via upload

---
+++ Global.js
@@ -0,0 +1,30 @@
+module.exports = {
+   
+    BASE_URL: 'http://192.168.43.239:1337/',
+    Styling: {
+      Colors: {
+          // Login and Sign-up pages
+          containerBackground: '#DCDCDC',
+          inputContainerBackground: '#FFFFFF' ,
+          inputContainerBorder: '#F5FCFF' ,
+          textInputBorder: '#FFFFFF',
+          buttonBackground: 'black',
+          buttonText: 'white',
+          // My Orders page
+          ordersTextLight: '#f78259',
+          ordersTextDark: '#eb4559',
+          cardContainerBorder: '#ddd',
+          cardContainerBackground: 'white',
+          modalContainerBackground: 'white',
+          // Products page
+          searchBarBackground: '#d3f4ff',
+          // Cart page
+          noItemViewBackground: 'white',
+          modalViewBackground: 'white',
+          totalViewBackground: '#fbe4d1',
+          checkoutViewBackground: '#f60404',
+          displayViewboxBackground: 'white',
+
+      }
+  }
+  };
---
+++ images.js
@@ -0,0 +1,8 @@
+const images = {
+    // Image for logo
+    Logo: require('./person.png'),
+    // Image for ChatBot Icons
+    UserIcon: require("./person.png"),
+    CPUIcon: require("./technical-support.png"),
+};
+export default images;
